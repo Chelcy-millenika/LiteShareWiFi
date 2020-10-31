7dc43f4c66d205795575ee499188d14ef6df870a
Chelcy Millenika <chelcymillenika840@gmail.com>
2020-11-01 03:27:20 +0530

    qr creat scanning and decode

--- app/src/main/java/com/cnnfe/liteshare/QRActivity.java
+++ app/src/main/java/com/cnnfe/liteshare/QRActivity.java
@@ -1,4 +1,135 @@
 package com.cnnfe.liteshare;
 
-public class QRActivity {
-}
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+
+public class QRActivity extends AppCompatActivity {
+
+    private static final int CAMERA_PERMISSION_CODE = 101;
+
+    private TextView textView;
+    private ImageView barcode;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        barcode = findViewById(R.id.bar_code);
+        textView = findViewById(R.id.data_text);
+
+        String data_in_code = "Hello Bar Code Data"; //this string value is being passed in qr
+
+        MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+        try {
+            BitMatrix bitMatrix = multiFormatWriter.encode(data_in_code, BarcodeFormat.QR_CODE, 200, 200);
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+            Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+            barcode.setImageBitmap(bitmap); //barcode will appear in image view
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //now let's create barcode scanner
+
+        Button scan_code = findViewById(R.id.button_scan);
+        scan_code.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT >= 23) {
+                    if (checkPermission(Manifest.permission.CAMERA)) {
+                        openScanner();
+                    } else {
+                        requestPermission(Manifest.permission.CAMERA, CAMERA_PERMISSION_CODE);
+                    }
+                } else {
+                    openScanner();
+                }
+            }
+        });
+
+
+    }
+
+
+
+
+    private void openScanner() {
+        new IntentIntegrator(QRActivity.this).initiateScan();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        IntentResult result=IntentIntegrator.parseActivityResult(requestCode,resultCode,data);
+        if(result!=null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "Blank", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                textView.setText("Data : "+result.getContents());
+            }
+        }
+        else{
+            Toast.makeText(this, "Blank", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean checkPermission(String permission){
+        int result= ContextCompat.checkSelfPermission(QRActivity.this,permission);
+        if(result== PackageManager.PERMISSION_GRANTED){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    private void requestPermission(String permision,int code){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(QRActivity.this,permision)){
+
+        }
+        else{
+            ActivityCompat.requestPermissions(QRActivity.this,new String[]{permision},code);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode){
+            case CAMERA_PERMISSION_CODE:
+                if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                    openScanner();
+                }
+        }
+    }
+
+    }
